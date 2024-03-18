79a07da7ed8de825348dc446f707e213c8e00672
Vijay Varadarajan <vijayvaradarajan84@gmail.com>
2024-03-19 04:00:01 +0530

    staticfiles mod

--- sociovate/settings.py
+++ sociovate/settings.py
@@ -157,7 +157,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 import os
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
