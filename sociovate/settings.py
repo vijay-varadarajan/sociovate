49334b028c773ed5757e17f07ce6237117faafa1
Vijay Varadarajan <vijayvaradarajan84@gmail.com>
2024-03-19 19:11:18 +0530

    completed footer

--- sociovate/settings.py
+++ sociovate/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
